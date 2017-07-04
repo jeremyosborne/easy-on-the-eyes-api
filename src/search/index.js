4f85c1dfde0db7b4d2d6dd28eaea7e7fab912c11
Jeremy Osborne <jeremywosborne@gmail.com>
2017-07-04 10:09:56 -0700

    Conform to JSON structure
    Might need more than one content structure, but for now I like the regularity.
    
    Perhaps instead of different base structures, can implement optional pieces (composition), or a 'condensed' structure.

--- src/search/index.js
+++ src/search/index.js
@@ -9,39 +9,39 @@ const get = require('lodash').get
  * objects.
  */
 module.exports.fetch = (query) => {
-  return axios.get('https://en.wikipedia.org/w/api.php?action=query&list=search&utf8&format=json&srsearch=' + encodeURIComponent(query))
+  const searchUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&utf8&format=json&srsearch=' + encodeURIComponent(query)
+  return axios.get(searchUrl)
     .then(function (response) {
       // console.log(JSON.stringify(response.data, null, 4))
-      return get(response, 'data.query.search', []).map((match) => {
-        return {
-          title: match.title,
-          type: 'text/html',
-          text: match.snippet,
-          href: '//wikipedia.org/wiki/' + encodeURIComponent(match.title),
+      return get(response, 'data.query.search', []).map((match) => (
+        {
+          error: {
+            code: 0,
+            message: ''
+          },
+          content: {
+            transformer: null,
+            title: match.title,
+            type: 'text/html',
+            text: match.snippet,
+            url: '//wikipedia.org/wiki/' + encodeURIComponent(match.title),
+          },
         }
-      })
-      // return content({
-      //   content: {
-      //     transformer: transformer.name,
-      //     url: url,
-      //     type: 'html',
-      //     text: transformer.xform(response.data).trim(),
-      //   },
-      // })
+      ))
     })
     .catch(function (err) {
-      // const code = err.response ? err.response.status : 500
-      // // Network error OR non-okay response.
-      // var error = content({
-      //   error: {
-      //     code: code,
-      //     message: 'Could not retrieve content',
-      //   },
-      //   content: {
-      //     transformer: transformer.name,
-      //     url: url,
-      //   },
-      // })
-      throw err
+      const code = err.response ? err.response.status : 500
+      // Network error OR non-okay response.
+      var error = {
+        error: {
+          code: code,
+          message: 'Could not retrieve content',
+        },
+        content: {
+          transformer: null,
+          url: searchUrl,
+        },
+      }
+      throw error
     })
 }
